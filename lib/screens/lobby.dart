e9dc26f0370dd9d6af23f0fac231cbaeeb7bc81e
comerc <andrew.kachanov@gmail.com>
2021-12-31 11:45:36 +0300

    Add ListUsers to LobbyScreen

--- lib/screens/lobby.dart
+++ lib/screens/lobby.dart
@@ -7,6 +7,25 @@ class LobbyScreen extends StatefulWidget {
   _LobbyScreenState createState() => _LobbyScreenState();
 }
 
+enum Status {
+  ready,
+  wait,
+}
+
+class User {
+  User({required this.name, required this.status});
+
+  String name;
+  Status status;
+}
+
+final data = [
+  User(name: "Andrei", status: Status.wait),
+  User(name: "Ruslan", status: Status.wait),
+  User(name: "Maxim", status: Status.wait),
+  User(name: "Sasha", status: Status.wait),
+];
+
 class _LobbyScreenState extends State<LobbyScreen> {
   @override
   Widget build(BuildContext context) {
@@ -14,11 +33,39 @@ class _LobbyScreenState extends State<LobbyScreen> {
       child: Column(
         children: [
           Center(child: Text("Users")),
-          Row(
-            children: [Text('Username')],
-          )
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: ListUsers(),
+          ),
         ],
       ),
+    );
+  }
+}
+
+class ListUsers extends StatefulWidget {
+  const ListUsers({Key? key}) : super(key: key);
+
+  @override
+  _ListUsersState createState() => _ListUsersState();
+}
+
+class _ListUsersState extends State<ListUsers> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+          itemCount: data.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Padding(
+                padding: const EdgeInsets.only(right: 10),
+                child: Text('${data[index].name}'),
+              ),
+              ElevatedButton(
+                  onPressed: () {}, child: Text("${data[index].status}"))
+            ]);
+          }),
     );
   }
 }

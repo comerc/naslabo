38744e3ac144b096ed2c9db4d4a01ad1eed87bfa
MaxMcRolly <m.prylutski@gmail.com>
2021-12-31 12:41:45 +0300

    Created MessageScreen

---
+++ lib/screens/message.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class MessageScreen extends StatelessWidget {
+  final messageController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Enter your message"),),
+        body: Column(
+          children: [
+            TextFormField(
+              controller: messageController,
+            ),
+            ElevatedButton(
+              onPressed: (){
+                Navigator.pop(context, "/lobby");
+              },
+              child: Text("Send your message"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

9ff7ed4cfe798531faf5f104146a19e974aa9775
comerc <andrew.kachanov@gmail.com>
2021-12-31 10:55:47 +0300

    Initial commit

---
+++ lib/screens/lobby.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Lobby extends StatefulWidget {
+  const Lobby({ Key? key }) : super(key: key);
+
+  @override
+  _LobbyState createState() => _LobbyState();
+}
+
+class _LobbyState extends State<Lobby> {
+  @override
+  Widget build(BuildContext context) {
+    return (
+      
+    );
+  }
+}

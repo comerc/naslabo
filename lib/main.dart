ba34c937efd091777e1fc406dcf39fe6706b82f1
MaxMcRolly <m.prylutski@gmail.com>
2021-12-31 12:33:54 +0300

    Added route /message

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       routes: {
         '/lobby': (context) => LobbyScreen(),
         '/login': (context) => LoginScreen(),
+        '/message':(context)=> MessageScreen
       },
       initialRoute: "/login",
       home: Scaffold(
